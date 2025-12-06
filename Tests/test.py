35268a91537ed49df3196a965df0315dfcadabe5
Khadija Elabbioui <khadijaelabbioui1999@gmail.com>
2025-12-06 11:54:58 +0100

    delete files from main

--- Tests/test.py
+++
@@ -1,8 +0,0 @@
-# def test (mocker):
-#     fake = mocker.Mock()
-#     fake.status_code == 200
-#     fake.json.return_value = {}
-#     mocker.patch("request.post",return_value = fake)
-
-#     result = query {}
-
--- api_app/schemas/Output_SR_gemini.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class GeminiResponse(BaseModel):
-    text_resume : str
-    ton : str
-
-    class Config:
-        from_attributes = True
-
