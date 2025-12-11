5bcc1a30caa8a0b4c6d5a1093381183e16f74011
Khadija Elabbioui <khadijaelabbioui1999@gmail.com>
2025-12-11 09:37:18 +0100

    modified:   Tests/chainage_complet.py

--- Tests/chainage_complet.py
+++ Tests/chainage_complet.py
@@ -7,13 +7,17 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
 
-from fastapi import FastAPI, Depends
-from api_app.main import app ,verify_token 
-# -------------------------------------------------------------
-# client = TestClient(app)
+from api_app.core.security import verify_token
+
+from api_app.main import app 
+
+
+
 @pytest.fixture
 def client() : 
     return TestClient(app)
+
+
 @pytest.fixture
 def fake_verify_token():
     
@@ -25,7 +29,7 @@ def test_analyse_endpoint_success(client, fake_verify_token, mocker):
     
     payload = {"text": "Nous devons renforcer la sécurité du serveur."}
 
-   # 2. Mock Hugging Face
+   #  Mock Hugging Face
     mock_hf = mocker.patch("api_app.services.analyse_text.ZS_Classify")
     mock_hf.return_value = {"categorie": "IT", "score": 0.9512}
 
@@ -43,17 +47,13 @@ def test_analyse_endpoint_success(client, fake_verify_token, mocker):
     assert data["score"] == 95.12
 
 
-def test_analyse_endpoint_auth_fail(client, mocker):
-    """Vérifie que sans l'override d'auth, ça bloque (401 ou 403)"""
+def test_analyse_auth_fail(client, mocker):
     
-    # On n'utilise PAS la fixture override_auth ici
-    # On s'assure qu'aucun override ne traîne
     app.dependency_overrides = {} 
 
     payload = {"text": "Test secret"}
     response = client.post("/analyse", json=payload)
 
-    # Si ça renvoie 404 ici, c'est que l'URL "/analyse" est fausse dans @app.post(...)
-    # Si ça renvoie 401/403, c'est bon (accès refusé)
-    assert response.status_code in [401, 403, 422] 
-    # Note: 422 arrive parfois si le token est attendu dans le body ou header manquant
+    # 401 (Token faux) ou 422 (Token absent/Header manquant)
+    assert response.status_code in [401, 422]
+    
